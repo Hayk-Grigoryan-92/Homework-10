8e4c0517413dd687082d4ebdeee38ecd9f7d82bc
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-10-03 19:35:31 +0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import { QuestionAnswer } from './components/questionAnswer';
 import bgImage from './assets/images/bg1.jpg'
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes, Link } from "react-router-dom";
 import { routerPages } from "./router"
 import { UserDetails } from "../src/components/userDetail";
 
@@ -16,19 +16,19 @@ function App() {
     <div className="App" style={{backgroundImage:`url(${bgImage})`}}>
       <button onClick={handleClick}>
       User Details
-      <NavLink to={routerPages.DETAILS}/>
+      <Link to={routerPages.DETAILS}/>
       </button>
       <button>
         Back 
-        <NavLink to={routerPages.HOME}/>
+        <Link to={routerPages.HOME}/>
       </button>
-      <QuestionAnswer/>
+  
 
 
 
       <Routes>
         <Route path={routerPages.DETAILS} element={<UserDetails />} />
-        {/* <Route path={routerPages.HOME} element={<QuestionAnswer />} /> */}
+        <Route path={routerPages.HOME} element={<QuestionAnswer />} />
       </Routes>
     </div>
   );
