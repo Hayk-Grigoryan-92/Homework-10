d3148f63a35663758751cc0b06f7fb3c77c858ed
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-10-03 19:41:16 +0400

    add navigate

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.scss';
 import { QuestionAnswer } from './components/questionAnswer';
 import bgImage from './assets/images/bg1.jpg'
-import { NavLink, Route, Routes, Link } from "react-router-dom";
+import { NavLink, Route, Routes, Link, useNavigate } from "react-router-dom";
 import { routerPages } from "./router"
 import { UserDetails } from "../src/components/userDetail";
 
 
-function handleClick (){
-  console.log('Vazgen');
-}
 
 function App() {
+
+  const navigate = useNavigate()
+
+  function handleClick (){
+    navigate(routerPages.DETAILS, {})
+  }
+
   return (
     <div className="App" style={{backgroundImage:`url(${bgImage})`}}>
       <button onClick={handleClick}>
